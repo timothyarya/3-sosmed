178532f40a28d61cb5ee61e38828bc753a40985c
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-07 13:10:42 +0700

    Add comment

--- index.ts
+++ index.ts
@@ -4,21 +4,34 @@ import logger from "./utils/logger";
 import UserRoute from "./routes/user.route";
 import PostRoute from "./routes/post.route";
 
+// Create an express application
 const app = express();
 
+// Enable CORS for the client
 app.use(
   cors({
+    // Allow credentials (cookies, authorization headers, etc.)
     credentials: true,
+    // Allow all headers
     allowedHeaders: ["*"],
+    // Allow all methods
     methods: ["*"],
+    // Only allow requests from the client
     origin: "localhost:3000",
   })
 );
+
+// Parse JSON bodies
 app.use(express.json());
+
+// Parse URL encoded bodies
 app.use(express.urlencoded({ extended: true }));
+
+// Define routes
 app.use("/api/v1/users", UserRoute);
 app.use("/api/v1/posts", PostRoute);
 
+// Start the server
 (async () => {
   app.listen(5000, () => logger.info("Server is running on port 5000!"));
 })();
