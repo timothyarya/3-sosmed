15570d2b56f831d96aa8943373c46a03fb3ceba9
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-07 07:33:39 +0700

    Added createUser, updateUser, searchUser

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -35,7 +35,10 @@ export const validateUsers = async (
   }
 };
 
-export const getUsers = async (req: Request, res: Response): Promise<any> => {
+export const getUserByID = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   const { id } = req.params;
 
   try {
@@ -51,6 +54,120 @@ export const getUsers = async (req: Request, res: Response): Promise<any> => {
         .json({ error: "User tidak terdaftar" });
     }
     return res.status(StatusCodes.OK).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getUsersByQuery = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  const { query } = req.query;
+
+  try {
+    if (!query) {
+      const users = await db.user.findMany();
+      return res.json(users);
+    }
+
+    const users = await db.user.findMany({
+      where: {
+        OR: [
+          {
+            username: query as string,
+          },
+          {
+            email: query as string,
+          },
+          {
+            nickname: query as string,
+          },
+        ],
+      },
+    });
+
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createUser = async (req: Request, res: Response): Promise<any> => {
+  const { username, nickname, password, confirmPassword, email } = req.body;
+
+  if (!username || !nickname || !password || !confirmPassword || !email)
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Bad data provided." });
+
+  if (username.length < 4)
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Minimum username length is 4" });
+
+  if (password !== confirmPassword)
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Confirmation password not valid." });
+
+  try {
+    // Check if user exists
+    const userExists = await db.user.findFirst({
+      where: {
+        OR: [
+          {
+            username,
+          },
+          {
+            email,
+          },
+        ],
+      },
+    });
+
+    if (userExists)
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ error: "User sudah terdaftar" });
+
+    // hash password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await db.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+        nickname,
+        email,
+      },
+    });
+
+    res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "new user registered", user: newUser });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateUser = async (req: Request, res: Response): Promise<any> => {
+  const { id } = req.params;
+
+  if (!req.body || !id)
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Need id or empty changes" });
+
+  try {
+    const user = await db.user.update({
+      where: {
+        id,
+      },
+      data: req.body,
+    });
+    res.json({ newData: user });
   } catch (error) {
     console.log(error);
   }
