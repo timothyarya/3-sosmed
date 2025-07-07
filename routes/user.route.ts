13c262939801ef91a1f05b80f9b6bac5d3109e0d
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-07 13:11:09 +0700

    Added photo routes

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -5,12 +5,17 @@ import {
   validateUsers, // Validate user credentials
   createUser, // Create new user
   updateUser, // Update existing user
+  getProfilePhoto,
+  setProfilePhoto,
 } from "../controllers/user.controller";
+import upload from "../utils/upload";
 
 const router = Router();
 
 router.get("/:id", getUserByID); // Get user by ID
+router.get("/photo/:id", getProfilePhoto);
 router.get("/validate", validateUsers); // Validate user credentials
+router.post("/photo/:id", upload.single("file"), setProfilePhoto);
 router.post("/", createUser); // Create new user
 router.patch("/:id", updateUser); // Update existing user
 
