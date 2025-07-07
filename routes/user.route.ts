d45e710a8a57e7a886e21f8c18285c2037270536
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-07 20:48:06 +0700

    Add logger and server.log

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -7,14 +7,17 @@ import {
   updateUser, // Update existing user
   getProfilePhoto,
   setProfilePhoto,
+  getUsers,
 } from "../controllers/user.controller";
 import upload from "../utils/upload";
+import logger from "../utils/logger";
 
 const router = Router();
 
-router.get("/:id", getUserByID); // Get user by ID
-router.get("/photo/:id", getProfilePhoto);
 router.get("/validate", validateUsers); // Validate user credentials
+router.get("/all", getUsers);
+router.get("/id/:id", getUserByID); // Get user by ID
+router.get("/photo/:id", getProfilePhoto);
 router.post("/photo/:id", upload.single("file"), setProfilePhoto);
 router.post("/", createUser); // Create new user
 router.patch("/:id", updateUser); // Update existing user
